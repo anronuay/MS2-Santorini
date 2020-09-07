bcd842eda48c35fb801c90d73af5ef7b95d1a072
anronuay <riv3r@msn.com>
2020-09-07 20:33:03 +0000

    Initial JS added to Google Map API

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,7 +1,28 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 36.3932, lng: 25.4615},
-    zoom: 11.5
+    center: {lat: 36.396132, lng: 25.461830},
+    zoom: 11.75
   });
 }
+
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+var locations = [
+        {lat: 36.405899, lng: 25.396495}
+      ]
+
+var markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+          });
+        });
+
+var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+
+
+
+
+
