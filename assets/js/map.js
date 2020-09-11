a077f9bea83c15e12ce701d86c4794bbdb48dd9f
anronuay <riv3r@msn.com>
2020-09-11 14:11:17 +0000

    Additional JS added to search bar

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,7 +1,7 @@
 function initAutocomplete() {
   const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 36.3932, lng: 25.4615 },
-    zoom: 11.5,
+    zoom: 15,
     mapTypeId: "roadmap",
     mapTypeControl: false
   });
