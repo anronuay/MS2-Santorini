155d47f2d0712461100c4524b2499647d68ec2ab
anronuay <riv3r@msn.com>
2020-09-12 17:48:27 +0000

    Amended some JS in the markers section

--- assets/js/map.js
+++ assets/js/map.js
@@ -71,62 +71,62 @@ function initAutocomplete() {
 
 var markers = [
     { // Amoudi Bay
-      latlong: { lat: 36.4600, lng: 25.3705 },
+      coords: { lat: 36.4600, lng: 25.3705 },
       iconImage: "assets/images/attractions-icon.png",
       content: "<img src='assets/images/map-images/amoudi-bay.jpg'><h5>Amoudi Bay</h5><a href='https://en.wikipedia.org/wiki/Amoudi_Bay' target='_blank'>Click here to learn more about Amoudi Bay</a>",
     },
     { // Nea Kameni
-      latlong: { lat: 36.4000, lng: 25.4000 },
+      coords: { lat: 36.4000, lng: 25.4000 },
       iconImage: "assets/images/attractions-icon.png",
       content: "<img src='assets/images/map-images/nea-kameni.jpg'><h5>Nea Kameni</h5><a href='https://en.wikipedia.org/wiki/Nea_Kameni' target='_blank'>Click here to learn more about Nea Kameni</a>",
     },
     { // Red Beach
-      latlong: { lat: 36.348705, lng: 25.394634 },
+      coords: { lat: 36.348705, lng: 25.394634 },
       iconImage: "assets/images/attractions-icon.png",
       content: "<img src='assets/images/map-images/red-beach.jpg'><h5>Red Beach</h5><a href='https://en.wikipedia.org/wiki/Red_Beach_(Santorini)' target='_blank'>Click here to learn more about Red Beach</a>",
     },
     { // Akrotiri Archaeological Site
-      latlong: { lat: 36.3518, lng: 25.4034 },
+      coords: { lat: 36.3518, lng: 25.4034 },
       iconImage: "assets/images/attractions-icon.png",
       content: "<img src='assets/images/map-images/akrotiri-archaeological-site.jpg'><h5>Akrotiri Archaeological Site</h5><a href='https://en.wikipedia.org/wiki/Akrotiri_(prehistoric_city)' target='_blank'>Click here to learn more about Akrotiri</a>",
     },
     { // Lombranos
-      latlong: { lat: 36.4173, lng: 25.4282 },
+      coords: { lat: 36.4173, lng: 25.4282 },
       iconImage: "assets/images/restaurant-icon.png",
       content: "<img src='assets/images/map-images/lombranos.jpg'><h5>Lombranos</h5><a href='https://www.facebook.com/Lombranos/' target='_blank'>Click here to learn more about Lombranos</a>",
     },
     { // The Wine Bar
-      latlong: { lat: 36.435923, lng: 25.421759 },
+      coords: { lat: 36.435923, lng: 25.421759 },
       iconImage: "assets/images/restaurant-icon.png",
       content: "<img src='assets/images/map-images/the-wine-bar.jpg'><h5>The Wine Bar</h5><a href='https://winebar-santorini.gr/' target='_blank'>Click here to learn more about The Wine Bar</a>",
     },
     { // Caldera Romantica
-      latlong: { lat: 36.367639, lng: 25.386075 },
+      coords: { lat: 36.367639, lng: 25.386075 },
       iconImage: "assets/images/restaurant-icon.png",
       content: "<img src='assets/images/map-images/caldera-romantica-restaurant.jpg'><h5>Caldera Romantica</h5><a href='https://www.calderaromantica.gr/' target='_blank'>Click here to learn more about Caldera Romantica</a>",
     },
     { // Enigma Cafe
-      latlong: { lat: 36.4174, lng: 25.4317 },
+      coords: { lat: 36.4174, lng: 25.4317 },
       iconImage: "assets/images/restaurant-icon.png",
       content: "<img src='assets/images/map-images/enigma-cafe.jpg'><h5>Enigma Cafe</h5><a href='https://www.facebook.com/enigmacafesantorini/' target='_blank'>Click here to learn more about Enigma Cafe</a>",
     },
     { // Fira
-      latlong: { lat: 36.4166, lng: 25.4324 },
+      coords: { lat: 36.4166, lng: 25.4324 },
       iconImage: "assets/images/accommodation-icon.png",
       content: "<img src='assets/images/map-images/fira.jpg'><h5>Fira</h5><a href='https://en.wikipedia.org/wiki/Fira' target='_blank'>Click here to learn more about Fira</a>",
     },
     { // Oia
-      latlong: { lat: 36.4618, lng: 25.3753 },
+      coords: { lat: 36.4618, lng: 25.3753 },
       iconImage: "assets/images/accommodation-icon.png",
       content: "<img src='assets/images/map-images/oia.jpg'><h5>Oia</h5><a href='https://en.wikipedia.org/wiki/Oia,_Greece' target='_blank'>Click here to learn more about Oia</a>",
     },
     { // Imerovigli
-      latlong: { lat: 36.4328, lng: 25.4228 },
+      coords: { lat: 36.4328, lng: 25.4228 },
       iconImage: "assets/images/accommodation-icon.png",
       content: "<img src='assets/images/map-images/imerovigli.jpg'><h5>Imerovigli</h5><a href='https://en.wikipedia.org/wiki/Imerovigli' target='_blank'>Click here to learn more about Imerovigli</a>",
     },
     { // Firostefani
-      latlong: { lat: 36.4252, lng: 25.4292 },
+      coords: { lat: 36.4252, lng: 25.4292 },
       iconImage: "assets/images/accommodation-icon.png",
       content: "<img src='assets/images/map-images/firostefani.jpg'><h5>Firostefani</h5><a href='https://www.santorini-island.com/firostefani.html' target='_blank'>Click here to learn more about Firostefani</a>",
     },
@@ -140,7 +140,7 @@ function addMarkers() {
 
   function addMarker(props) {
     var marker = new google.maps.Marker({
-      position: props.latlong,
+      position: props.coords,
       map: map,
     });
 
